4e02bc34cc2135139e4b6b6f31f3679d17c6d0bc
sarkar157112 <69862614+sarkar157112@users.noreply.github.com>
2022-05-03 14:01:57 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
 variable "access_key" {
-  default = "AKIAQSEAXFL6OZAFN6IK"
+  default = "AKIAQSEAXFL6OZAF6K"
   }
 variable "secret_key" {
-  default = "KtJXJL9F+0zopw190yuNw4AKX935/iICTiNQasji"
+  default = "KtJXJL9F+0zopwyuNw4AKX935/iICTiNQasji"
   }
 variable "region" {
   default = "us-east-1"
